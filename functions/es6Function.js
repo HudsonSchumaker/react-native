f3e5364f7bef5b0e774d965887506fdb2fc3b6ce
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-02-02 09:29:54 +0100

    Update es6Function.js

--- functions/es6Function.js
+++ functions/es6Function.js
@@ -1,6 +1,7 @@
 // Hudson Schumaker
 
 // Using ES6 arrow function
-const Hello = () => {
-  return <h1>Hello World</h1>;
-}
+function Hello = greeting => { greeting }
+
+// Function call
+hello('Hello Wolrd'); // returns Hello World
